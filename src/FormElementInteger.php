1078c3c4273e9489d79988a80667ce1253cd9c29
Freek Lijten <freek.lijten@procurios.nl>
2012-05-23 09:48:41 +0200

    Add comments

--- src/FormElementInteger.php
+++ src/FormElementInteger.php
@@ -1,9 +1,14 @@
 <?php
 namespace src;
 use src\FormElementBase;
+use src\ValidatorInteger;
 
 class FormElementInteger extends FormElementBase
 {
+	/**
+	 * Returns an array of FormElementValidator Objects
+	 * @return array
+	 */
 	public function getValidators()
 	{
 		$validators = parent::getValidators();
