dfe04b6ec73984fea2a72b434b13e86144cb19f2
Freek Lijten <freek.lijten@procurios.nl>
2012-05-24 09:01:08 +0200

    Extended index.php with new concepts

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php
+namespace src;
 require_once __DIR__ . '/src/AutoLoader.php';
 \src\AutoLoader::register();
 
-$Form = new \src\Form();
-$Form->addFormElement( new \src\FormElementText('Test') );
-$FormElementInteger = new \src\FormElementInteger('Integer');
-$FormElementInteger->setValue('asd');
+$Form = new \src\Form('testForm');
+$Form->addFormElement( new \src\FormElements\FormElementText('Test') );
+$FormElementInteger = new \src\FormElements\FormElementInteger('Integer');
+$FormElementInteger->setValue(123);
 $Form->addFormElement( $FormElementInteger );
-
+$Form->addFormElement( new \src\FormElements\FormElementSubmit('Versturen!') );
 echo $Form;
 
-var_dump($Form->isValid());
-
+if ($Form->isSubmitted()) {
+	r($_REQUEST);
+}
